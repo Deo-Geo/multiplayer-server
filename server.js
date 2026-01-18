a537302ba4dda92573a3e5dae6d5ddf2bc3c1910
Deo-Geo <deqn7654321@gmail.com>
2026-01-19 00:19:42 +0200

    Fix CORS and update server URL

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://projectballs.netlify.app",
     methods: ["GET", "POST"]
   }
 })
